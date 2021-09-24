f08cf9835ebf1b717d36dc54e29c81388902919d
sanishchirayath1 <sanishchirayath@gmail.com>
2021-09-24 23:53:55 +0530

    quicksort

--- scripts/quick.js
+++ scripts/quick.js
@@ -1,8 +1,9 @@
-async function quickSort() {
+async function quickSort(array, start, end) {
     const elements = document.querySelectorAll(".bars");
     length = elements.length;
+    if (parseInt(elements[start].style.height) < parseInt(elements[end].style.height)){
 
-    
+    }
 }
 const quickBtn = document.getElementById("quick-sort");
 
