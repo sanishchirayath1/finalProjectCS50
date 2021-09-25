cadf090e84a444771e3c3a9b85e11b82e8cbf728
sanishchirayath1 <sanishchirayath@gmail.com>
2021-09-25 18:07:15 +0530

    s

--- scripts/merge.js
+++ scripts/merge.js
@@ -1,17 +1,16 @@
-// async function insertionSort() {
-//     const elements = document.querySelectorAll(".bars");
-//     length = elements.length;
+const mergeBtn = document.getElementById("merge-sort");
+
+mergeBtn.addEventListener("click", async function() {
+    await mergeSort();
+})
+
+async function mergeSort() {
+    const elements = document.querySelectorAll(".bars");
+    length = elements.length;
     
-//     if(parseInt(elements[j].style.height) >= parseInt(elements[j+1].style.height)){
-//         await wait(delay);
-//         swap(elements[j], elements[j + 1]);
-//     }
     
-// }
+    
+}
 
 
-// const mergeBtn = document.getElementById("insertion-sort");
 
-// mergeBtn.addEventListener("click", async function() {
-//     await insertionSort();
-// })
--- scripts/quick.js
+++ scripts/quick.js
@@ -1,12 +1,38 @@
-async function quickSort(array, start, end) {
-    const elements = document.querySelectorAll(".bars");
-    length = elements.length;
-    if (parseInt(elements[start].style.height) < parseInt(elements[end].style.height)){
-
-    }
-}
 const quickBtn = document.getElementById("quick-sort");
 
 quickBtn.addEventListener("click", async function() {
-    await insertionSort();
+    await quickSort();
 })
+
+// main function
+async function quickSort(start, end, array) {
+    const elements = document.querySelectorAll(".bars");
+    length = elements.length; 
+    if (start < end){
+        p = partition(start,end,array)
+
+        quickSort(start, end, array)
+        quickSort(p + 1, end, array)
+    }
+}
+// partition function
+function partition(start, end, array){
+    pivot_index = star
+    pivot = array[pivot_index]
+
+    while(start < end){
+        
+        while(start < array.length && array[start] <= pivot){
+            start += 1
+        }
+        while (array[end] > pivot) {
+            end -= 1
+        }
+
+        if(start < end){
+            swap(array[start], array[end])
+        }
+    }
+    swap(array[end], array[pivot_index])
+    return end
+}
